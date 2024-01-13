6e06d38d209f85b8076785b943eac0aa14dd0c07
Natnicha <nat.rodtong@gmail.com>
2024-01-13 20:58:10 +0100

    feat: add a request model to register

--- _backend/auth/auth.py
+++ _backend/auth/auth.py
@@ -1,7 +1,18 @@
+from fastapi import APIRouter
+from auth.model import RegisterRequestModel
 
-from fastapi import Header, APIRouter
 auth = APIRouter()
 
 @auth.post("/register")
-async def register():
-    return {"message": "This is a register route"}
+async def register(item: RegisterRequestModel):
+    # get across university domains
+    # check email format
+    # if not conform email format, return error
+    # check whether or not a given email is one of Across uni
+    # if no - not match with any uni, return error
+    # check existing
+    # if yes - exists, return error
+    # extract first_name & last_name 
+    # sent email
+    # if success, insert into db & return success
+    return {"message": item}
