a6de05fb77ce5df2dcf0fa7548eb0cf56d446f4e
Natnicha <nat.rodtong@gmail.com>
2024-01-14 22:12:38 +0100

    feat: add logic checking whether or not a requested domain is under Across uni partner

--- _backend/api/auth/auth.py
+++ _backend/api/auth/auth.py
@@ -19,10 +19,16 @@ async def register(
     if not isCorrectEmailFormat:
         return {"message": "The email doesn't conform by email format, please input in format of example@university.de"}
 
+    # extract domain from a request
+    request_domain = extract_domain_from_email(item.email)
     # get across university domains
+    email_domains_collection = db.get_database("admin").get_collection("email_domains").find({"domain" : request_domain})
+    
     # check whether or not a given email is one of Across uni
     # if no - not match with any uni, return error
-    
+    if len(list(email_domains_collection)) == 0:
+        return {"message": "The email's domain isn't under Across, please input another email which is under Across"}
+
     # check existing
     # if yes - exists, return error
     # extract first_name & last_name 
@@ -39,3 +45,6 @@ def validate_email(email):
         return True
     else:
         return False
+
+def extract_domain_from_email(email):
+    return email.split("@",1)[1]
