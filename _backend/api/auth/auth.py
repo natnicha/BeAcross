edd1f56ad4ad3b8c2c2154c5982c1bf22dcf7ca4
Natnicha <nat.rodtong@gmail.com>
2024-01-15 17:11:21 +0100

    amend: shorten code on preparing and insert new user for /register

--- _backend/api/auth/auth.py
+++ _backend/api/auth/auth.py
@@ -30,29 +30,15 @@ async def register(
     password = generatePassword()
     print("Only temporary show password, will be deleted when email server is ready.")
     print("Generated password is:"+password)
+
     # encrypt password using salted hashing
     encrypted_password = hashText(password)
     
     # TODO: send email
     
     # if sent success, insert into db & return 200 - OK 
-    last_name = ''
-    if len(full_name)>0:
-        last_name = full_name[1]
+    return prepare_and_insert_user(db, full_name, item.email, encrypted_password)
 
-    new_user = UsersModel(
-        email = item.email,
-        password = encrypted_password,
-        first_name = full_name[0],
-        last_name = last_name,
-    )
-
-    try:
-        db.get_database("admin").get_collection("users").insert_one(new_user.dict())
-    except:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    return new_user
 
 def check_email_format(email):
     isCorrectEmailFormat = validate_email(email)
@@ -77,10 +63,29 @@ def check_across_partner(db, email):
         )
 
 def check_user_existing(db, email):
-    users_collection = db.get_database("admin").get_collection("users").find({"email" : item.email})
+    users_collection = db.get_database("admin").get_collection("users").find({"email" : email})
     # if yes - exists, return error
     if len(list(users_collection)) > 0:
         raise HTTPException( 
             detail={"message": "The email is already taken, please check again"},
             status_code=status.HTTP_400_BAD_REQUEST
         )
+
+def prepare_and_insert_user(db, full_name, email, password):
+    last_name = ''
+    if len(full_name)>0:
+        last_name = full_name[1]
+
+    new_user = UsersModel(
+        email = email,
+        password = password,
+        first_name = full_name[0],
+        last_name = last_name,
+    )
+
+    try:
+        db.get_database("admin").get_collection("users").insert_one(new_user.dict())
+    except:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    return new_user
