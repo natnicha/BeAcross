c8b9bb6822b407f1a2e9fac5b32732ee58fae7d8
Natnicha <nat.rodtong@gmail.com>
2024-01-15 14:41:13 +0100

    fix: fix UsersModel to align with schema

--- _backend/db/users.py
+++ _backend/db/users.py
@@ -1,13 +1,17 @@
 from typing import Optional
 from bson import ObjectId
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, Field
+
+class BaseModel(BaseModel):
+    class Config:
+        arbitrary_types_allowed = True
 
 class UsersModel(BaseModel):
     id: Optional[ObjectId] = Field(..., alias="_id")
-    email: EmailStr = Field(...)
+    email: str = Field(...)
     password: bytes = Field(...)
     first_name: str = Field(...)
     last_name: str = Field(...)
-    registration_number: str = Field(...)
+    registration_number: int = Field(...)
     course_of_study: str = Field(...)
-    semester: str = Field(...)
+    semester: int = Field(default=1)
