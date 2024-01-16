74ea212ab8932d2023ee2a759ecc8c070d0eba44
Natnicha <nat.rodtong@gmail.com>
2024-01-16 22:49:26 +0100

    fix: improve calling database from conn.get_database(env_config.DB_NAME) to conn[env_config.DB_NAME]

--- _backend/app/crud/email_domains.py
+++ _backend/app/crud/email_domains.py
@@ -4,4 +4,4 @@ from pymongo import MongoClient
 from app.config.config_utils import env_config
 
 def get_email_domain(conn: MongoClient, request_domain: str):
-    return conn.get_database(env_config.DB_NAME).get_collection("email_domains").find({"domain" : request_domain})
+    return conn[env_config.DB_NAME].get_collection("email_domains").find({"domain" : request_domain})
