37441e1722fb73fb37313dc877401f881322cc1b
Natnicha <nat.rodtong@gmail.com>
2024-01-17 15:54:07 +0100

    feat: add prepare model and insert into module_recommend collection

--- _backend/app/api/module/module.py
+++ _backend/app/api/module/module.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, status
 from mongomock import MongoClient
 
+import app.crud.module_recommend as MODULE_RECOMMEND
+from app.crud.module_recommend import ModuleRecommendModel
 from app.db.mongodb import get_database
 from app.api.module.model import RecommendRequestModel
 
@@ -12,4 +14,8 @@ async def register(
         item: RecommendRequestModel = None,
         db: MongoClient = Depends(get_database),
     ):
+    module_recommend = ModuleRecommendModel(
+        module_id=item.module_id
+    )
+    MODULE_RECOMMEND.insert_one(db, module_recommend)
     return 
