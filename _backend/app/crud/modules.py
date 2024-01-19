a0737742d539c9bc008de392681f9d8c9cbc9de9
Natnicha <nat.rodtong@gmail.com>
2024-01-19 10:57:54 +0100

    feat: add find logic into /module/search

--- _backend/app/crud/modules.py
+++ _backend/app/crud/modules.py
@@ -1,16 +1,23 @@
 import datetime
 import decimal
+<<<<<<< HEAD
 import re
 from typing import Optional
 from bson import ObjectId
 from mongomock import MongoClient
 from pydantic import BaseModel, Field
 from bson.objectid import ObjectId as BsonObjectId
+=======
+from typing import Optional
+from mongomock import MongoClient
+from pydantic import BaseModel, Field
+>>>>>>> 6fa551a (feat: add find logic into /module/search)
 from app.config.config_utils import env_config
 
 class BaseModel(BaseModel):
     class Config:
         arbitrary_types_allowed = True
+<<<<<<< HEAD
         allow_population_by_field_name = True
         json_encoders = {ObjectId: str}
 
@@ -22,6 +29,16 @@ class ModulesModel(BaseModel):
     university: str = Field(...)
     ect_credits: decimal = Field(...)
     year_of_study: decimal = Field(...)
+=======
+
+class UserLogsModel(BaseModel):
+    name: str = Field(...)
+    program: str = Field(...)
+    degree_level: str = Field(...)
+    university: str = Field(...)
+    ect_credits: decimal = Field(...)
+    semester: decimal = Field(...)
+>>>>>>> 6fa551a (feat: add find logic into /module/search)
     content: str = Field(...)
     created_at: Optional[datetime.datetime] = Field(default=datetime.datetime.utcnow())
     updated_at: Optional[datetime.datetime] = Field(default=datetime.datetime.utcnow())
@@ -31,6 +48,7 @@ def find(conn: MongoClient, term: str, limit: int, offset: int, sortby: str, ord
     if orderby.lower() == 'desc':
         is_asc = -1
 
+<<<<<<< HEAD
     like_term = convert_to_like(term=term)
     return conn[env_config.DB_NAME].get_collection("modules").find({
         "name": like_term
@@ -46,3 +64,10 @@ def count(conn: MongoClient, term: str):
 
 def convert_to_like(term: str):
     return re.compile('.*'+term+'.*', re.IGNORECASE)
+=======
+    return conn[env_config.DB_NAME].get_collection("modules").find({
+        "name": term
+    }).sort({
+        sortby : int(is_asc)
+    }).skip(offset).limit(limit)
+>>>>>>> 6fa551a (feat: add find logic into /module/search)
