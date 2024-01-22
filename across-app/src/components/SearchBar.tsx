563b6df4cb15f4d7323e39a61721b3508dd8c06c
pjitthamma <ExcoSia@LIVE.RU>
2024-01-22 20:29:08 +0100

    add operator dropdown

--- across-app/src/components/SearchBar.tsx
+++ across-app/src/components/SearchBar.tsx
@@ -9,6 +9,16 @@ interface SearchBarProps {
 const SearchBar: React.FC<SearchBarProps> = ({ content, setContent }) => {
     
  const navigate = useNavigate();
+ const [showAdvanceSearch, setAdvanceSearch] = useState(false); // State to manage visibility of the advance search panel
+ const [selectedValue, setSelectedValue] = useState('AND');
+
+ const AdvanceSearchClick = () => {
+    setAdvanceSearch(true);
+ };
+
+ const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedValue(event.target.value);
+  };
     
     return (
         <>
@@ -34,19 +44,42 @@ const SearchBar: React.FC<SearchBarProps> = ({ content, setContent }) => {
                     </button>
                 </div>
 
-                <div className="advancedSearchRow">
-                    <a
-                        className="click-scroll d-flex align-items-end"
-                        href="javascript:void(0)"
-                        onClick={(e) => {
-                            e.preventDefault(); // Prevent default if using href="#"
-                        }}
-                        role="button"
-                        tabIndex={0}
-                        >
-                        <strong><u>Advance Search</u></strong>
-                    </a>
-                </div>
+                    <div className="advancedSearchRow">
+                        <a
+                            className="click-scroll d-flex align-items-end"
+                            href="javascript:void(0)"
+                            onClick={(e) => {
+                                e.preventDefault(); // Prevent default if using href="#"
+                                AdvanceSearchClick();
+                            }}
+                            role="button"
+                            tabIndex={0}
+                            >
+                            <strong><u>Advance Search Panel</u></strong>
+                            &nbsp;<i className="bi bi-caret-right-fill"></i>
+                        </a>
+                    {showAdvanceSearch && (
+                        <>
+                            <select
+                                name="logicalOperators"
+                                id="logicalOperators"
+                                value={selectedValue}
+                                onChange={handleChange}
+                            >
+                                <option value="AND">AND</option>
+                                <option value="OR">OR</option>
+                                <option value="NOT">NOT</option>
+                            </select>
+                            <input
+                                type="text"
+                                className="searchInput"
+                                placeholder="Search module here..."
+                                value={content}
+                                onChange={(e) => setContent(e.target.value)} 
+                            />
+                        </>                   
+                    )}
+                    </div>
             </div>
         </section>
         </>
