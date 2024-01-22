0cc8796edcf401f3be71b1861386b55ab3c8260f
pjitthamma <ExcoSia@LIVE.RU>
2024-01-22 09:19:35 +0100

    fix valuable frontend_origins

--- _backend/main.py
+++ _backend/main.py
@@ -17,7 +17,7 @@ frontend_origins = ['http://localhost:3000']
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=frontend_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
