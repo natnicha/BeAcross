32fd5a6c109f38115b69013ab4636120f1c6565a
pjitthamma <ExcoSia@LIVE.RU>
2024-01-22 13:26:59 +0100

    add tenplate search result list

--- across-app/src/pages/SearchPage.tsx
+++ across-app/src/pages/SearchPage.tsx
@@ -1,15 +1,52 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 
-const SearchPage = () => {
+const SearchPage: React.FC = (props) => {
   const location = useLocation();
   const result = location.state.content;
 
   return (
-    <div>
-      <h1>Search Page</h1>
-      <h3>{result}</h3>
-    </div>
+    <>
+      {/*Search title*/}
+      <section className="projects section-padding pb-0">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 text-left mb-4" style={{ display: 'flex', alignItems: 'center' }}>
+              <h2 style={{ display: "inline" }}>Browsing within...&nbsp;&nbsp;</h2> 
+              <h2 style={{ display: "inline", color: "#1e5af5" , marginRight: "auto"}}>{result}</h2>
+
+              <p style={{ margin: 0 }}>Number of results found: 64</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      
+      {/*Filter*/}
+      <section></section>
+
+      {/*Search list*/}
+      <section>
+      <div className="search-header">
+        <div className="search-column"><strong>Image</strong></div>
+        <div className="search-column"><strong>Details</strong></div>
+      </div>
+      
+      <div className="search-table">
+        <div className="search-row">
+            <div className="search-column">
+                image
+            </div>
+            <div className="search-column">
+                <strong>Place Name</strong><br></br>
+                Description or Address<br></br>
+                Rating: ★★★★☆
+            </div>
+        </div>
+      </div>
+
+      </section>
+
+    </>
   );
 };
 
