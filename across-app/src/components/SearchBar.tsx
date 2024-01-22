a51a023bffb9e3c474982ec07a73d09119108535
pjitthamma <ExcoSia@LIVE.RU>
2024-01-22 20:12:06 +0100

    add advance searh button

--- across-app/src/components/SearchBar.tsx
+++ across-app/src/components/SearchBar.tsx
@@ -14,25 +14,40 @@ const SearchBar: React.FC<SearchBarProps> = ({ content, setContent }) => {
         <>
        {/*Searchbar*/}
         <section className="searchbar">
-
-                <div className="searchbarContent">
-                <i className="bi bi-search"></i>
-                <input
-                    type="text"
-                    className="searchInput"
-                    placeholder="Search module here..."
-                    value={content}
-                    onChange={(e) => setContent(e.target.value)}
-                />&nbsp;&nbsp;
-                <button
-                    className="custom-btn btn custom-link"
-                    onClick={() =>
-                        navigate("/search?query=" + content, { state: { content } })
-                      }
-                >
-                    Search
-                </button>
+            <div className="searchbarContent">
+                <div className="searchbarRow">
+                    <i className="bi bi-search"></i>
+                    <input
+                        type="text"
+                        className="searchInput"
+                        placeholder="Search module here..."
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                    />&nbsp;&nbsp;
+                    <button
+                        className="custom-btn btn custom-link"
+                        onClick={() =>
+                            navigate("/search?query=" + encodeURIComponent(content), { state: { content } })
+                        }
+                    >
+                        Search
+                    </button>
                 </div>
+
+                <div className="advancedSearchRow">
+                    <a
+                        className="click-scroll d-flex align-items-end"
+                        href="javascript:void(0)"
+                        onClick={(e) => {
+                            e.preventDefault(); // Prevent default if using href="#"
+                        }}
+                        role="button"
+                        tabIndex={0}
+                        >
+                        <strong><u>Advance Search</u></strong>
+                    </a>
+                </div>
+            </div>
         </section>
         </>
     );
