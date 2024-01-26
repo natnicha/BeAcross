21aa181b562f9798396117852f214e45348e20ba
Natnicha <nat.rodtong@gmail.com>
2024-01-27 00:02:45 +0100

    feat: rename email template and add more excited content register email template

---
+++ _backend/app/email_service/email_templates.py
@@ -0,0 +1,38 @@
+
+password_reset_template = """
+Dear {user},
+
+Your password has been reset. Your new password is: {password}
+
+Please change it after your next login.
+
+Wishing you well,
+Your Victory Pie Solutions Team
+"""
+
+
+registration_template = """
+Hi, {user}!
+
+Welcome to Across. Thank you for registering with us. 
+We're excited to have you on board and can't wait to help you get started.
+
+Let us do the work for you to plan studying across countries in Europe!
+
+To start your journey, please user details below:
+Login email: {email}
+Your new password is: {password}
+
+Ready to organize for a new chapter of your study life?
+<a href="http://localhost:3000/">Across</a>
+
+Wishing you well,
+Your Victory Pie Solutions Team
+
+
+Remark
+- Please keep your account information safe.
+- This E-mail has been automatically sent by the system, please do not reply back. If you wish to contact us,
+please send E-mail to <a href="Victorypiesolutions@outlook.com">Victorypiesolutions@outlook.com</a>
+
+"""
