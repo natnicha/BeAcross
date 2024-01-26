993dcaf9fff5f134593a9b0e92c551c98e162578
pjitthamma <ExcoSia@LIVE.RU>
2024-01-26 15:17:56 +0100

    modify seachService to recieved filter

--- across-app/src/services/searchServices.ts
+++ across-app/src/services/searchServices.ts
@@ -17,37 +17,62 @@ export interface SearchResponse {
     total_results?: number;
     items?: SearchItem[];
 }
+
+interface FilterParams {
+    degreeLevels: string[];
+    moduleTypes: string[];
+    universities: string[];
+    ectsCredits: number;
+  }
  
 // Function to send a GET request to the search API
-export async function searchServices(term: string, offset: number): Promise<SearchResponse> {
+export async function searchServices(term: string, offset: number, filter: FilterParams): Promise<SearchResponse> {
     
-    let baseUrl = 'http://localhost:8000/api/v1/module/search?term=';
+    // Base URL including the term
+    let url = `http://localhost:8000/api/v1/module/search?term=${encodeURIComponent(term)}`;
+
+    // Add offset to the URL if it's not 0
+    if (offset !== 0) {
+        url += `&offset=${offset}`;
+    }
+
+    // Add filters to the URL
+    if (filter.degreeLevels.length > 0) {
+        url += `&degree_level=${encodeURIComponent(filter.degreeLevels.join(','))}`;
+    }
+    if (filter.moduleTypes.length > 0) {
+        url += `&module_type=${encodeURIComponent(filter.moduleTypes.join(','))}`;
+    }
+    if (filter.universities.length > 0) {
+        url += `&universitie=${encodeURIComponent(filter.universities.join(','))}`;
+    }
+    if (filter.ectsCredits > 0) {
+        url += `&ects=${filter.ectsCredits}`;
+    }
     
-    let url = offset === 0 ? `${baseUrl}${term}` : `${baseUrl}${term}&offset=${offset}`;
     try {
         const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
         });
-
-        // Parse the JSON response
+    
         const responseData: any = await response.json();
-
+    
         if (response.ok) {
-            return { 
-                total_items: responseData.data.total_items, 
-                total_results: responseData.data.total_results, 
-                items: responseData.data.items,
-            };
+          return {
+            total_items: responseData.data.total_items,
+            total_results: responseData.data.total_results,
+            items: responseData.data.items,
+          };
         } else if (responseData.detail && responseData.detail.message === "No module found") {
-            return { message: responseData.detail.message };
+          return { message: responseData.detail.message };
         } else {
-            throw new Error(`Error: ${response.status}`);
+          throw new Error(`Error: ${response.status}`);
         }
-    } catch (error) {
+      } catch (error) {
         console.error('Error fetching data from search:', error);
         throw error;
-    }
+      }
 };
