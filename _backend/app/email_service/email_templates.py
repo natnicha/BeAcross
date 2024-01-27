ac6337417fbdacc9713c213812453105bb88700a
Natnicha <nat.rodtong@gmail.com>
2024-01-27 20:36:35 +0100

    fix: change text color in email registering template

--- _backend/app/email_service/email_templates.py
+++ _backend/app/email_service/email_templates.py
@@ -13,20 +13,20 @@ Your Victory Pie Solutions Team
 
 registration_template = u"""
 <html>
-    <head>Hi <font color="#f75940">{user}!</font></head>
+    <head>Hi <font color="#1e5af5">{user}!</font></head>
     <body font-family: 'Source Sans Pro'>
         <br>
-        Welcome to <font color="#f75940">Across</font>. Thank you for registering with us. <br>
+        Welcome to <font color="#1e5af5">Across</font>. Thank you for registering with us. <br>
         We're excited to have you on board and can't wait to help you get started.<br>
         <br>
         <b>Let us do the work for you to plan studying across countries in Europe!</b><br>
         <br>
         To start your journey, please use details below:<br>
-        Login email: <font color="#f75940">{email}</font><br>
-        Your password is: <font color="#f75940">{password}</font><br>
+        Login email: <font color="#1e5af5">{email}</font><br>
+        Your password is: <font color="#1e5af5">{password}</font><br>
         <br>
         <b>Ready to organize your new chapter of study life ?</b><br>
-        <b><a href="http://localhost:3000/"><font color="#f75940">Across</font></a></b><br>
+        <b><a href="http://localhost:3000/"><font color="#1e5af5">Across</font></a></b><br>
         <br>
         <font style="font-size:14px">Wishing you well,</font><br>
         <font style="font-size:14px">Your Victory Pie Solutions Team</font><br><br><br>
