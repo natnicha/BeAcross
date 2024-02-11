4771d7e2652e89a14f9a756d6aa389bdbc6ea820
salehghraiyib <saleh.ghrayeb@gmail.com>
2024-02-11 18:37:15 +0100

    Similarity after module insert - owl file update

--- _backend/app/api/module/module.py
+++ _backend/app/api/module/module.py
@@ -6,6 +6,7 @@ from fastapi import APIRouter, Depends, HTTPException, Header, Query, Request, s
 from pymongo import MongoClient
 from pymongo.cursor import Cursor
 from owlready2 import *
+from app.owl.modules import add_modules_to_owl
 import xml.etree.ElementTree as ET
 
 import app.crud.module_recommend as MODULE_RECOMMEND
@@ -17,20 +18,23 @@ from app.crud.module_comment import ModuleCommentModel
 from app.db.mongodb import get_database
 from app.api.module.model import *
 
-from app.transferability.similiarity_run import start_similarity_for_one
+from app.transferability.similiarity_run import start_similarity_for_one, add_module_to_res
 
-from app.owl.modules import find_suggested_modules
+from app.owl.modules import find_suggested_modules, delete_file
 
 module = APIRouter()
 
-sortby_database_col_mapping = {"module_name":"name", "degree_program":"degree_program", "degree_level":"degree_level", "ects":"ects", "university":"university", "module_type": "type", "module_code": "module_code", "content": "content"}
+sortby_database_col_mapping = {"module_name": "name", "degree_program": "degree_program",
+                               "degree_level": "degree_level", "ects": "ects", "university": "university",
+                               "module_type": "type", "module_code": "module_code", "content": "content"}
+
 
 @module.post("/recommend", status_code=status.HTTP_201_CREATED)
 async def recommend(
         request: Request,
         item: RecommendRequestModel = None,
         db: MongoClient = Depends(get_database),
-    ):
+):
     try:
         module_recommend = ModuleRecommendModel(
             module_id=ObjectId(item.module_id),
@@ -44,7 +48,8 @@ async def recommend(
 
     check_conflict(db, module_recommend)
     insert_module_recommend(db, module_recommend)
-    return 
+    return
+
 
 def check_conflict(db: MongoClient, module_recommend: ModuleRecommendModel):
     rows = MODULE_RECOMMEND.get_module_recommend(db, module_recommend)
@@ -53,6 +58,7 @@ def check_conflict(db: MongoClient, module_recommend: ModuleRecommendModel):
             detail={"message": "this action is performed"},
             status_code=status.HTTP_409_CONFLICT
         )
+
 
 def insert_module_recommend(db: MongoClient, module_recommend: ModuleRecommendModel):
     try:
@@ -63,11 +69,12 @@ def insert_module_recommend(db: MongoClient, module_recommend: ModuleRecommendMo
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
         )
 
+
 @module.get("/{module_id}/recommend", response_model=CountRecommendResponseModel)
 async def no_of_recommend(
         module_id: str = None,
         db: MongoClient = Depends(get_database),
-    ):
+):
     module_id_obj = None
     try:
         module_id_obj = ObjectId(module_id)
@@ -84,6 +91,7 @@ async def no_of_recommend(
             "no_of_recommend": str(count)
         }
     )
+
 
 def get_no_of_recommend_module(db: MongoClient, module_id: ObjectId):
     count = 0
@@ -102,7 +110,7 @@ async def delete_recommend(
         request: Request,
         module_id: str = None,
         db: MongoClient = Depends(get_database),
-    ):
+):
     try:
         module_recommend = ModuleRecommendModel(
             module_id=ObjectId(module_id),
@@ -116,7 +124,8 @@ async def delete_recommend(
 
     check_exist_module_recommend(db, module_recommend)
     delete_module_recommend(db, module_recommend)
-    return 
+    return
+
 
 def check_exist_module_recommend(db: MongoClient, module_recommend: ModuleRecommendModel):
     rows = MODULE_RECOMMEND.get_module_recommend(db, module_recommend)
@@ -125,6 +134,7 @@ def check_exist_module_recommend(db: MongoClient, module_recommend: ModuleRecomm
             detail={"message": "the module recommended by this user is not found"},
             status_code=status.HTTP_404_NOT_FOUND
         )
+
 
 def delete_module_recommend(db: MongoClient, module_recommend: ModuleRecommendModel):
     try:
@@ -141,7 +151,7 @@ async def comment(
         request: Request,
         items: ModuleCommentRequestModel,
         db: MongoClient = Depends(get_database),
-    ):
+):
     try:
         module_comment = ModuleCommentModel(
             module_id=ObjectId(items.module_id),
@@ -160,9 +170,10 @@ async def comment(
         message=module_comment.message,
         user_id=module_comment.user_id)
     return ModuleCommentResponseModel(
-        message = "successful commented",
-        data = data
+        message="successful commented",
+        data=data
     )
+
 
 def insert_module_comment(db: MongoClient, module_comment: ModuleCommentModel):
     try:
@@ -179,7 +190,7 @@ async def delete_comment(
         request: Request,
         module_comment_id: str = None,
         db: MongoClient = Depends(get_database),
-    ):
+):
     try:
         module_comment_id_obj = ObjectId(module_comment_id)
         user_id_obj = ObjectId(request.state.user_id)
@@ -190,7 +201,8 @@ async def delete_comment(
         )
     check_exist_module_comment(db, module_comment_id_obj, user_id_obj)
     delete_module_comment(db, module_comment_id_obj, user_id_obj)
-    return 
+    return
+
 
 def check_exist_module_comment(db: MongoClient, module_comment_id: ObjectId, user_id: ObjectId):
     rows = MODULE_COMMENT.find(db, module_comment_id, user_id)
@@ -200,6 +212,7 @@ def check_exist_module_comment(db: MongoClient, module_comment_id: ObjectId, use
             status_code=status.HTTP_404_NOT_FOUND
         )
 
+
 def delete_module_comment(db: MongoClient, module_comment_id: ObjectId, user_id: ObjectId):
     try:
         MODULE_COMMENT.delete_one(db, module_comment_id, user_id)
@@ -208,7 +221,7 @@ def delete_module_comment(db: MongoClient, module_comment_id: ObjectId, user_id:
             detail={"message": e},
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
         )
-    
+
 
 @module.get("/search/", status_code=status.HTTP_200_OK)
 async def search(
@@ -219,18 +232,19 @@ async def search(
         module_type: Annotated[Union[list[str], None], Query()] = None,
         limit: int = Query(20, gt=0),
         offset: int = Query(0, gt=0),
-        sortby: str = Query('module_name', pattern='^module_name$|^degree_program$|^no_of_recommend$|^no_of_suggested_modules$|^degree_level$|^ects$|^university$|^module_type$'),
+        sortby: str = Query('module_name',
+                            pattern='^module_name$|^degree_program$|^no_of_recommend$|^no_of_suggested_modules$|^degree_level$|^ects$|^university$|^module_type$'),
         orderby: str = Query('asc', pattern='^asc$|^desc$'),
         db: MongoClient = Depends(get_database),
-    ):
+):
     count = MODULES.count(db, term, degree_level, ects, university, module_type)
     if count == 0:
         raise HTTPException(detail={"message": "No module found"}, status_code=status.HTTP_404_NOT_FOUND)
-    
+
     sortby_column = 'module_name'
     if not is_manual_calculated_sortby(sortby=sortby):
-        sortby_column = sortby_database_col_mapping[sortby] 
-    
+        sortby_column = sortby_database_col_mapping[sortby]
+
     items = MODULES.find(db, term, degree_level, ects, university, module_type, limit, offset, sortby_column, orderby)
     data = prepare_item(db, items)
 
@@ -242,7 +256,8 @@ async def search(
             "total_results": count,
             "total_items": len(data),
             "items": data
-    }}
+        }}
+
 
 def prepare_item(db: MongoClient, items: Cursor):
     data = parse_json(items)
@@ -255,29 +270,33 @@ def prepare_item(db: MongoClient, items: Cursor):
         entry['no_of_suggested_modules'] = len(OWL_MODULES.find_suggested_modules(entry["module_id"]))
     return data
 
+
 def sort(data: list, sortby: str, orderby: str):
     is_reverse = False
     if orderby.lower() == 'desc':
         is_reverse = True
     return sorted(data, reverse=is_reverse, key=lambda d: d[sortby])
 
+
 def is_manual_calculated_sortby(sortby: str):
     return (sortby == 'no_of_recommend' or sortby == 'no_of_suggested_modules')
+
 
 def parse_json(data):
     return json.loads(json_util.dumps(data))
 
+
 @module.post("/", status_code=status.HTTP_201_CREATED)
 async def create_module(
-    request: Request,
-    content_type: str = Header(...),
-    db: MongoClient = Depends(get_database)):
+        request: Request,
+        content_type: str = Header(...),
+        db: MongoClient = Depends(get_database)):
     if content_type != "application/xml":
         raise HTTPException(
             status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
             detail={
                 "message": f"Unsupported media type: {content_type}."
-                " It must be application/xml"
+                           " It must be application/xml"
             }
         )
 
@@ -288,36 +307,39 @@ async def create_module(
         raise HTTPException(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
             detail={"message": "Expected tag not found or unexpected tag found. Please check you XML file tags",
-                "hint": str(e)},
+                    "hint": str(e)},
         )
-    
+
     inserted_modules = MODULES.insert_many(db, db_modules_list)
     item_response_list = []
     for index, element in enumerate(uploaded_modules_list):
         module_id = str(inserted_modules.inserted_ids[index])
         item = UploadModulesResponseItemModel(
-                module_id=module_id,
-                module_name=element.name,
-                degree_program=element.degree_program,
-                degree_level=element.degree_level,
-                university=element.university,
-                module_code=element.module_code,
-                content=element.content,
-                ects=int(element.ects or 0),
-                type=element.type
-            )
+            module_id=module_id,
+            module_name=element.name,
+            degree_program=element.degree_program,
+            degree_level=element.degree_level,
+            university=element.university,
+            module_code=element.module_code,
+            content=element.content,
+            ects=int(element.ects or 0),
+            type=element.type
+        )
         item_response_list.append(item)
 
-    # TODO: call similarity calculation function
+    for mod in item_response_list:
+        add_module_to_res(mod.module_id)
+
     for mod in item_response_list:
         start_similarity_for_one(mod)
         similar_module_id_list = find_suggested_modules(mod.module_id)
 
-        # TODO: get module detail for each similar module
         similar_module_detail_list = []
         for similar_module_id in similar_module_id_list:
             similar_module_detail_list.append(MODULES.find_one(db, ObjectId(similar_module_id)))
         mod.similar_modules = similar_module_detail_list
+
+    add_modules_to_owl()
 
     return {
         "data": {
@@ -326,7 +348,8 @@ async def create_module(
         }
     }
 
-def get_data_from_xml(text: bytes) -> (list, list) :
+
+def get_data_from_xml(text: bytes) -> (list, list):
     tree = ET.ElementTree(ET.fromstring(text))
     modules_graph = tree.getroot()
     uploaded_modules_list = []
--- _backend/app/owl/modules.py
+++ _backend/app/owl/modules.py
@@ -13,6 +13,9 @@ def find_suggested_modules(module: str) -> list:
     # Navigate to the 'owl' directory and access 'results.json'
     owl_path = os.path.join(backend_directory, "owl", "modules.owl")
 
+    if not os.path.isfile(owl_path):
+        return []
+
     onto = get_ontology(owl_path).load()
     # Get the individual by its name
     individual = onto.search_one(iri="*%s" % module)
@@ -24,7 +27,18 @@ def find_suggested_modules(module: str) -> list:
                 res.append(i.name)
     return res
 
+def delete_file():
+    current_directory = os.path.dirname(os.path.abspath(__file__))
 
+    # Jump up two parent directories to the '_backend' directory
+    backend_directory = os.path.dirname(os.path.dirname(current_directory))
+
+    # Navigate to the 'owl' directory and access 'results.json'
+    owl_path = os.path.join(backend_directory, "app", "owl", "modules.owl")
+
+    if os.path.isfile(owl_path):
+        print("File exists")
+        os.remove(owl_path)
 
 def add_modules_to_owl():
     # Get the current working directory (your_script.py's directory)
@@ -46,6 +60,10 @@ def add_modules_to_owl():
     fp.close()
 
     onto = get_ontology(owl_path).load()
+
+    # Remove all classes, individuals, properties, and axioms
+    for entity in list(onto.classes()) + list(onto.individuals()) + list(onto.properties()):
+        destroy_entity(entity)
 
     # define ontology classes
     with onto:
