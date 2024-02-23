a985967e85dc3b8974c9267bbc6065f2b3522657
Natnicha <nat.rodtong@gmail.com>
2024-02-23 14:35:36 +0100

    fix: remove module_id from get personal_plan

--- _backend/app/api/personal_plan/personal_plan.py
+++ _backend/app/api/personal_plan/personal_plan.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter, status
 
 personal_plan = APIRouter()
 
-@personal_plan.get("/{module_id}/", status_code=status.HTTP_200_OK)
+@personal_plan.get("/", status_code=status.HTTP_200_OK)
 async def get_personal_plan():
     return
