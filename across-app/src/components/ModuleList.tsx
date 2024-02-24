259bd820a853a4fbb7c394c9c23ad7becb2d00a5
Eun Young Choi <eun-young.choi@s2022.tu-chemnitz.de>
2024-02-24 17:42:46 +0100

    Add call module api

--- across-app/src/components/ModuleList.tsx
+++ across-app/src/components/ModuleList.tsx
@@ -1,21 +1,22 @@
 import { useEffect, useState } from "react";
 
 interface ModuleItem {
-  content: string;
-  university: string;
-  degree_program: string;
-  module_code: number;
-  ects: string;
-  degree_level: string;
-  module_name: string;
-  type: string;
+  university?: string;
+  degree_program?: string;
+  module_code?: number;
+  ects?: string;
+  degree_level?: string;
+  module_name?: string;
+  type?: string;
 }
 
 interface ModuleData {
-  message: string;
-  total_items: number;
-  total_results: number;
-  items: ModuleItem[];
+  data: {
+    message?: string;
+    total_items?: number;
+    total_results?: number;
+    items?: ModuleItem[];
+  };
 }
 
 export default function ModuleList() {
@@ -24,18 +25,21 @@ export default function ModuleList() {
   const jwtToken = sessionStorage.getItem("jwtToken");
 
   useEffect(() => {
-    fetch("http://localhost:8000/api/v1/user/profile/list", {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${jwtToken}`,
-      },
-    })
+    fetch(
+      `http://localhost:8000/api/v1/module/search/advanced?term=("university":Chemnitz)`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${jwtToken}`,
+        },
+      }
+    )
       .then((response) => response.json())
       .then((data: ModuleData) => {
         setModuleDatas(data);
       })
       .catch((error) => {
-        console.error("Error fetching studunt list data:", error);
+        console.error("Error fetching module list data:", error);
       });
   }, []);
 
@@ -64,43 +68,36 @@ export default function ModuleList() {
           <strong>University</strong>
         </div>
       </div>
-      {/*Display No Item*/}
-      {!moduleData?.items && (
-        <h2 style={{ textAlign: "center", color: "red", marginTop: "15px" }}>
-          {" "}
-          {moduleData && moduleData.message}{" "}
-        </h2>
-      )}
       {/*Display Items*/}
       {moduleData ? (
         <div className="search-table">
-          {moduleData.items.map((item, index) => (
-            <div className="search-row" key={index}>
-              <div className="search-column" id="moduleCode">
-                {item.module_code}
+          {moduleData.data.items &&
+            moduleData.data.items.map((item, index) => (
+              <div className="search-row" key={index}>
+                <div className="search-column" id="moduleCode">
+                  {item.module_code}
+                </div>
+                <div className="search-column" id="moduleName">
+                  {item.module_name}
+                </div>
+                <div className="search-column" id="ect">
+                  {item.ects}
+                </div>
+                <div className="search-column" id="degree">
+                  {item.degree_level}
+                </div>
+                <div className="search-column" id="type">
+                  {item.type}
+                </div>
+                <div className="search-column" id="university">
+                  {item.university}
+                </div>
               </div>
-              <div className="search-column" id="moduleName">
-                {item.module_name}
-              </div>
-              <div className="search-column" id="ect">
-                {item.ects}
-              </div>
-              <div className="search-column" id="degree">
-                {item.degree_level}
-              </div>
-              <div className="search-column" id="type">
-                {item.type}
-              </div>
-              <div className="search-column" id="university">
-                {item.university}
-              </div>
-            </div>
-          ))}
+            ))}
         </div>
       ) : (
         <p>No data</p>
       )}
-      ;
     </div>
   );
 }
