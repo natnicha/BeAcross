c884913cf951ebffe89bf61c35ff00e7c7c216b1
pjitthamma <ExcoSia@LIVE.RU>
2024-03-02 22:06:33 +0100

    add api call getModule

--- across-app/src/services/personalplanServices.ts
+++ across-app/src/services/personalplanServices.ts
@@ -24,6 +24,28 @@ interface PersonalPlan {
     is_added: boolean;
 }
 
+export interface moduleItem {
+    content?: string;
+    university?: string;
+    degree_program?: string;
+    module_code?: number;
+    ects?: string;
+    degree_level?: string;
+    module_name?: string;
+    type?: string;
+    no_of_recommend: number;
+    no_of_suggested_modules?: number;
+    module_id: string;
+    is_recommended: boolean;
+} 
+
+export interface ModuleResponse {
+    message?: string;
+    total_items?: number;
+    total_results?: number;
+    items?: moduleItem[];
+}
+
 // Function to send a POST request to the createPersonal API
 export async function postPersonalPlan(module_id: string, semester_id: string): Promise<PostPersonalPlanResponse> {
     const url: string = `http://localhost:8000/api/v1/personal-plan`;
@@ -62,9 +84,39 @@ export async function postPersonalPlan(module_id: string, semester_id: string): 
 }
 
 // Function to send a GET request to the getPersonal API
-export async function getPersonalPlan(module_id: string, token: string): Promise<PersonalPlanResponse> {
+export async function getPersonalPlan(module_id: string): Promise<PersonalPlanResponse> {
     const url: string = `http://localhost:8000/api/v1/module/${module_id}/comment`;
   
+    const token = sessionStorage.getItem("jwtToken") || '';
+
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+      });
+  
+      const responseData = await response.json();
+  
+      if (response.ok) {
+        // Assuming responseData directly matches the structure of getCommentResponse
+        return responseData;
+      } else {
+        throw new Error(`Error: ${response.status}`);
+      }
+    } catch (error) {
+      console.error('Error fetching data from search:', error);
+      throw error;
+    }
+  }
+
+// Function to send a GET request to the Module API
+export async function getModuleDetail(module_id: string): Promise<ModuleResponse> {
+    const url: string = `http://localhost:8000/api/v1/module/${module_id}`;
+  
+    const token = sessionStorage.getItem("jwtToken") || '';
     try {
       const response = await fetch(url, {
         method: 'GET',
