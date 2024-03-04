db300a0de108ed2105d5758a7fdd40adec112e83
Natnicha <nat.rodtong@gmail.com>
2024-03-04 19:08:05 +0100

    feat: add middleware utils test

---
+++ _backend/tests/middleware/test_middleware_utils.py
@@ -0,0 +1,42 @@
+from fastapi.testclient import TestClient
+from app.middleware.middleware_utils import is_public_path
+
+from app.config.config_utils import load_env
+from app.api.auth.auth import get_database
+from main import app
+
+client = TestClient(app)
+
+def test_is_public_path_root():
+    api = '/'
+    method = 'GET'
+    assert is_public_path(api, method)
+
+def test_is_public_path_auth():
+    api = '/api/v1/auth'
+    method = 'POST'
+    assert is_public_path(api, method)
+
+def test_is_public_path_docs():
+    api = '/docs'
+    method = 'GET'
+    assert is_public_path(api, method)
+
+def test_is_public_path_openapi():
+    api = '/openapi.json'
+    method = 'GET'
+    assert is_public_path(api, method)
+
+def test_is_public_path_module_search():
+    api = '/api/v1/module/search'
+    method = 'GET'
+    assert is_public_path(api, method)
+
+def test_is_public_path_get_module_recommend():
+    api = 'api/v1/module/65a8041efbc5863974a6d4e4/recommend'
+    method = 'GET'
+    assert is_public_path(api, method)
+
+    api = 'api/v1/module/65a8041efbc5863974a6d4e5/recommend'
+    method = 'GET'
+    assert is_public_path(api, method)
