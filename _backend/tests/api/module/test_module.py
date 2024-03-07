b3df203352addce9051d61a7b486d41ea9dd5a6c
Natnicha <nat.rodtong@gmail.com>
2024-03-07 21:23:10 +0100

    feat: add test_search_advanced_student_success_without_recommend

--- _backend/tests/api/module/test_module.py
+++ _backend/tests/api/module/test_module.py
@@ -732,6 +732,41 @@ def test_search_advanced_guest_success(mocker):
     assert response.json()["data"]["items"][0]["no_of_suggested_modules"]  == len(suggested_modules)
     assert response.json()["data"]["items"][0]["is_recommended"]  == False
 
+def test_search_advanced_uni_admin_success(mocker):
+    load_env()
+    init_setting()
+    mocker.patch('app.crud.modules.advanced_count', return_value=1)
+    modules = [{
+        "_id" : ObjectId("65ac17b1d2815b505f3e3557"),
+        "name" : "Parallel programming",
+        "content" : "Content:The content of the lecture includes: Architecture and connection networks of parallel systems; Performance, runtime analysis and scalability of parallel programs; Message passing programming and implementation of typical communication patterns; Programming and synchronization techniques for shared address space with multi-threading; Coordination of parallel programs. In the exercises, programming models and techniques are practically applied to various applications. Aim:Knowledge of the architecture and network structures of parallel platforms; Knowledge of basic programming techniques for shared and distributed address spaces and their application to various applications",
+        "program" : "Applied Computer Science",
+        "university" : "Technische Universitat Chemnitz",
+        "degree_program" : "Applied Computer Science",
+        "module_code" : "561070",
+        "ects" : 5,
+        "degree_level" : "Master"
+    }]
+    mocker.patch('app.crud.modules.advanced_find', return_value=modules)
+    mocker.patch('app.crud.module_recommend.count_module_recommend', return_value=58)
+
+    suggested_modules = ["65ac1847d2815b505f3e3b96", "65ac1847d2815b505f3e3b95"]
+    mocker.patch('app.owl.modules.find_suggested_modules', return_value=suggested_modules)
+    response = client.get(
+        url=f'/api/v1/module/search/advanced?term=("university":Chemnitz)',
+        headers={"Content-Type":"application/json", "Authorization": f"Bearer {uni_admin_jwt}"}
+    )
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["data"]["total_results"]  == len(modules)
+    assert response.json()["data"]["total_items"]  == len(modules)
+    assert response.json()["data"]["items"][0]["module_name"]  == modules[0]["module_name"]
+    assert response.json()["data"]["items"][0]["content"] == modules[0]["content"]
+    assert response.json()["data"]["items"][0]["program"]  == modules[0]["program"]
+    assert response.json()["data"]["items"][0]["university"]  == modules[0]["university"]
+    assert response.json()["data"]["items"][0]["no_of_recommend"]  == 58
+    assert response.json()["data"]["items"][0]["no_of_suggested_modules"]  == len(suggested_modules)
+    assert response.json()["data"]["items"][0]["is_recommended"]  == False
+
 def test_search_advanced_student_success_with_recommend(mocker):
     load_env()
     init_setting()
