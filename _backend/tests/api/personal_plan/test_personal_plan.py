af197b85e1fe068ef0f9e489a049b16d11437d74
Natnicha <nat.rodtong@gmail.com>
2024-03-07 22:00:24 +0100

    feat: add test_get_personal_plan_sys_admin_forbidden

--- _backend/tests/api/personal_plan/test_personal_plan.py
+++ _backend/tests/api/personal_plan/test_personal_plan.py
@@ -36,3 +36,12 @@ def test_get_personal_plan_uni_admin_forbidden():
         headers={"Content-Type":"application/json", "Authorization": f"Bearer {uni_admin_jwt}"}
     )
     assert response.status_code == status.HTTP_403_FORBIDDEN
+
+def test_get_personal_plan_sys_admin_forbidden():
+    load_env()
+    init_setting()
+    response = client.get(
+        url="/api/v1/personal-plan",
+        headers={"Content-Type":"application/json", "Authorization": f"Bearer {sys_admin_jwt}"}
+    )
+    assert response.status_code == status.HTTP_403_FORBIDDEN
