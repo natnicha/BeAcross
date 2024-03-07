0e2ec8308e757d3cbc7ea2d3d5cd10a937a0e83a
Natnicha <nat.rodtong@gmail.com>
2024-03-07 10:58:25 +0100

    feat: add test_module_recommend_module_id_incorrect_format

--- _backend/tests/api/module/test_module.py
+++ _backend/tests/api/module/test_module.py
@@ -69,3 +69,17 @@ def test_module_recommend_sys_admin_forbidden():
         json={}
     )
     assert response.status_code == status.HTTP_403_FORBIDDEN
+
+def test_module_recommend_module_id_incorrect_format():
+    load_env()
+    Settings.user_roles = {
+        "student": ObjectId("65a8040bfbc5863974a6d4e2"), 
+        "uni-admin": ObjectId("65a80418fbc5863974a6d4e3"), 
+        "sys-admin": ObjectId("65a8041efbc5863974a6d4e4"), 
+    }
+    response = client.post(
+        url="/api/v1/module/recommend",
+        headers={"Content-Type":"application/json", "Authorization": f"Bearer {student_jwt}"},
+        json={"module_id":"abcd"}
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
