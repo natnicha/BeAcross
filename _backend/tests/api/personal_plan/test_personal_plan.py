d0acbd68a09824fa2ecfbc2a20b32112610a8337
Natnicha <nat.rodtong@gmail.com>
2024-03-07 21:59:05 +0100

    feat: add test_get_personal_plan_unauthorized

---
+++ _backend/tests/api/personal_plan/test_personal_plan.py
@@ -0,0 +1,29 @@
+from bson import ObjectId
+from fastapi.testclient import TestClient
+from fastapi import status
+
+from app.db.settings import Settings
+from app.config.config_utils import load_env
+from main import app
+
+client = TestClient(app)
+
+student_jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjVlOGM3OTA0YThjM2MyMmJmODM5NTY5Iiwicm9sZSI6InN0dWRlbnQiLCJ1bml2ZXJzaXR5IjoiVGVjaG5pc2NoZSBVbml2ZXJzaXRhdCBDaGVtbml0eiIsImlhdCI6MTcwOTc1NjM4MSwiZXhwIjoyMDI1MTE2MzgxfQ.c7UeyBYA3jZj7ne6HejwA8wU3XMw75k7N3co3k0aL3E"
+uni_admin_jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjVlNGQyMmJhMjFkMzA4ZWNhMGM1MzFkIiwicm9sZSI6InVuaS1hZG1pbiIsInVuaXZlcnNpdHkiOiJUZWNobmlzY2hlIFVuaXZlcnNpdGF0IENoZW1uaXR6IiwiaWF0IjoxNzA5ODA1MDYzLCJleHAiOjIwMjUxNjUwNjN9.NTlNeqRgG1bDuEJEZoutHPACf8PQcnq_RHMKLNze-vo"
+sys_admin_jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjVlOThlMzZjOTRmNjU3ODNjM2JhZDRjIiwicm9sZSI6InN5cy1hZG1pbiIsInVuaXZlcnNpdHkiOiJUZWNobmlzY2hlIFVuaXZlcnNpdGF0IENoZW1uaXR6IiwiaWF0IjoxNzA5ODA1MTcwLCJleHAiOjIwMjUxNjUxNzB9.Dh1m07NfibQ9m3m7d_geowNpDwwkqtrJCaVqlW6vzjk"
+
+def init_setting():
+    Settings.user_roles = {
+        "student": ObjectId("65a8040bfbc5863974a6d4e2"), 
+        "uni-admin": ObjectId("65a80418fbc5863974a6d4e3"), 
+        "sys-admin": ObjectId("65a8041efbc5863974a6d4e4"), 
+    }
+
+def test_get_personal_plan_unauthorized():
+    load_env()
+    init_setting()
+    response = client.get(
+        url="/api/v1/personal-plan",
+        headers={"Content-Type":"application/json"}
+    )
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
