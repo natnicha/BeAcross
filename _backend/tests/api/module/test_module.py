f07c1ba5e1a1153dd6cbc527299c929be5b262e4
Natnicha <nat.rodtong@gmail.com>
2024-03-07 10:56:15 +0100

    feat: add test_module_recommend_uni_admin_forbidden, test_module_recommend_sys_admin_forbidden

--- _backend/tests/api/module/test_module.py
+++ _backend/tests/api/module/test_module.py
@@ -11,6 +11,8 @@ from main import app
 client = TestClient(app)
 
 student_jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjVlOGM3OTA0YThjM2MyMmJmODM5NTY5Iiwicm9sZSI6InN0dWRlbnQiLCJ1bml2ZXJzaXR5IjoiVGVjaG5pc2NoZSBVbml2ZXJzaXRhdCBDaGVtbml0eiIsImlhdCI6MTcwOTc1NjM4MSwiZXhwIjoyMDI1MTE2MzgxfQ.c7UeyBYA3jZj7ne6HejwA8wU3XMw75k7N3co3k0aL3E"
+uni_admin_jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjVlNGQyMmJhMjFkMzA4ZWNhMGM1MzFkIiwicm9sZSI6InVuaS1hZG1pbiIsInVuaXZlcnNpdHkiOiJUZWNobmlzY2hlIFVuaXZlcnNpdGF0IENoZW1uaXR6IiwiaWF0IjoxNzA5ODA1MDYzLCJleHAiOjIwMjUxNjUwNjN9.NTlNeqRgG1bDuEJEZoutHPACf8PQcnq_RHMKLNze-vo"
+sys_admin_jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjVlOThlMzZjOTRmNjU3ODNjM2JhZDRjIiwicm9sZSI6InN5cy1hZG1pbiIsInVuaXZlcnNpdHkiOiJUZWNobmlzY2hlIFVuaXZlcnNpdGF0IENoZW1uaXR6IiwiaWF0IjoxNzA5ODA1MTcwLCJleHAiOjIwMjUxNjUxNzB9.Dh1m07NfibQ9m3m7d_geowNpDwwkqtrJCaVqlW6vzjk"
 
 def test_post_no_request_body():
     load_env()
@@ -26,7 +28,7 @@ def test_post_no_request_body():
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.json()["message"] == "no request body provided"
 
-def test_module_recommend_unauthorized():
+def test_module_recommend_no_authorization():
     load_env()
     Settings.user_roles = {
         "student": ObjectId("65a8040bfbc5863974a6d4e2"), 
@@ -39,3 +41,31 @@ def test_module_recommend_unauthorized():
         json={}
     )
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+def test_module_recommend_uni_admin_forbidden():
+    load_env()
+    Settings.user_roles = {
+        "student": ObjectId("65a8040bfbc5863974a6d4e2"), 
+        "uni-admin": ObjectId("65a80418fbc5863974a6d4e3"), 
+        "sys-admin": ObjectId("65a8041efbc5863974a6d4e4"), 
+    }
+    response = client.post(
+        url="/api/v1/module/recommend",
+        headers={"Content-Type":"application/json", "Authorization": f"Bearer {uni_admin_jwt}"},
+        json={}
+    )
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+def test_module_recommend_sys_admin_forbidden():
+    load_env()
+    Settings.user_roles = {
+        "student": ObjectId("65a8040bfbc5863974a6d4e2"), 
+        "uni-admin": ObjectId("65a80418fbc5863974a6d4e3"), 
+        "sys-admin": ObjectId("65a8041efbc5863974a6d4e4"), 
+    }
+    response = client.post(
+        url="/api/v1/module/recommend",
+        headers={"Content-Type":"application/json", "Authorization": f"Bearer {sys_admin_jwt}"},
+        json={}
+    )
+    assert response.status_code == status.HTTP_403_FORBIDDEN
