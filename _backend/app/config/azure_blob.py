c44414e1603ca9b68a180e425e7cdf84fdf86d72
salehghraiyib <saleh.ghrayeb@gmail.com>
2024-03-07 16:45:28 +0100

    read file - basic

--- _backend/app/config/azure_blob.py
+++ _backend/app/config/azure_blob.py
@@ -3,6 +3,20 @@ from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 from app.config.config_utils import env_config
 from azure.storage.blob import BlobServiceClient
+import json
 
 def init():
     blob_service_client = BlobServiceClient.from_connection_string(env_config.AZURE)
+    print("\nListing blobs...")
+    container_name = "results"
+    blob_name = "result.json"
+
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+    # List the blobs in the container
+    blob_content = blob_client.download_blob().readall()
+
+    json_data = json.loads(blob_content)
+
+    # Now json_data is a dictionary containing the contents of the JSON blob
+    print(json_data)
+    
