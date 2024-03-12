300e638e4869511ed846c9710f7784a6334b2381
Natnicha <nat.rodtong@gmail.com>
2024-03-12 12:32:38 +0100

    feat: add validate email in contact-us

--- _backend/app/api/contact_us/contact_us.py
+++ _backend/app/api/contact_us/contact_us.py
@@ -1,11 +1,18 @@
-from fastapi import APIRouter, Depends, Request, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from pymongo import MongoClient
 
 from app.db.mongodb import get_database
+from app.api.auth.auth_utils import validate_email
+from app.api.contact_us.module import ContactUsRequest
 
 contact_us = APIRouter()
 @contact_us.post("/", status_code=status.HTTP_200_OK)
 async def post_contact_us(
+        items: ContactUsRequest,
         db: MongoClient = Depends(get_database)
     ):
+    is_valid_email = validate_email(items.email)
+    if not is_valid_email:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email is invalid format")
+    
     pass
